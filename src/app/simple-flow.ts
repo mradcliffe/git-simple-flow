41ad6a5d3dc47efb25e98b52b598891c3113797a
Matthew Radcliffe <mradcliffe@softpixel.com>
2021-07-08 11:15:33 -0400

    Removes fastforward to clean up graph,

--- src/app/simple-flow.ts
+++ src/app/simple-flow.ts
@@ -1,6 +1,6 @@
 import { GitgraphUserApi } from '@gitgraph/core';
 
-const simpleFlow = (graph: GitgraphUserApi<any>) => {
+const simpleFlow = (graph: GitgraphUserApi<any>): GitgraphUserApi<any> => {
   const main = graph.branch('main');
 
   const development = graph.branch('development');
@@ -9,17 +9,18 @@ const simpleFlow = (graph: GitgraphUserApi<any>) => {
   zbp0001.commit('ZBP-0001 Merge Request squashed');
   zbp0002.commit('ZBP-0002 Merge Request squashed');
 
-  development.merge({ branch: zbp0002, fastForward: true });
-  development.merge({ branch: zbp0001, fastForward: true });
+  development.merge(zbp0002);
+  development.merge(zbp0001);
 
-  const release1 = development.branch('release/2021-09-01');
-  const zbp0003 = release1.branch('bugfix/ZBP-0003');
+  const release1 = development.branch('release/2021-09-01').commit();
+
+  const zbp0003 = release1.branch('bugfix/ZBP-0003').commit();
   zbp0003.commit('ZBP-0003 Merge Request squashed');
-  release1.merge({ branch: zbp0003, fastForward: true });
+  release1.merge(zbp0003);
 
-  const zbp0004 = development.branch('feature/ZBP-0004');
+  const zbp0004 = development.branch('feature/ZBP-0004').commit();
   zbp0004.commit('ZBP-0004: Merge Request squashed');
-  development.merge({ branch: zbp0004, fastForward: true });
+  development.merge(zbp0004);
 
   main.merge(release1, 'Merges release/2021-09-01 into origin/main');
   development.merge(main);
@@ -30,7 +31,7 @@ const simpleFlow = (graph: GitgraphUserApi<any>) => {
 
   const zbp0006 = development.branch('feature/ZBP-0006');
   zbp0006.commit('ZBP-0006: Merge Request squashed');
-  development.merge({ branch: zbp0006, fastForward: true });
+  development.merge(zbp0006);
 
   development.merge(main);
 
@@ -39,10 +40,12 @@ const simpleFlow = (graph: GitgraphUserApi<any>) => {
   zbp0007.commit('ZBP-0007: Hot fix');
   main.merge(zbp0007, 'Merges hotfix/ZBP-0007 into origin/main');
 
-  // Release 2 doesn't include zbp0007 hot fix.
+  // Release 2 doesn't include zbp0007 hot fix yet.
   const release2 = development.branch('release/2021-09-10').commit();
-  main.merge(release2, 'Merges release/2021-09-10 into origin/main');
 
+  development.merge(main);
+  release2.merge(development);
+  main.merge(release2, 'Merges release/2021-09-10 into origin/main');
   development.merge(main);
 
   return graph;
